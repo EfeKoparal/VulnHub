2d9c8ba732d80badd89649365963b9648b646637
Efe Koparal <hesaplarozgurlukicin1@proton.me>
2022-07-12 21:46:52 +0300

    Create decryptpass.py

---
+++ fristileaks1.3/decryptpass.py
@@ -0,0 +1,14 @@
+# simdi ise cryptpass.py dosyasini tek tek inceleyerek onun tam tersiniz yazmaya calisacagiz. kendisi oncelikle kodumuzu base64 ile
+# encode ediyor ardindan ise ters cevirip rot13 ile encode ediyor o zaman yapilmasi gerekenler tam tersleri:
+# decode rot13 > ters cevir > decode base64
+
+import base64,codecs,sys
+
+def decodeString(str):
+    decode13=codecs.decode(str, 'rot13') # decode rot13
+    ters=decode13[::-1] # ters cevir
+    son=base64.b64decode(ters) # decode base64
+    return son
+sonuc=decodeString(sys.argv[1])
+print(sonuc)
+
